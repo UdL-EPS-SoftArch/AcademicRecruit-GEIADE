0c4b4af92f0ea768e7c8284c956200e7d12c875f
bilaluali <bilaluali98@gmail.com>
2021-04-29 22:12:05 +0200

    Defined selection process entity types

---
+++ src/app/selection-process/selection-process.ts
@@ -0,0 +1,13 @@
+import { Resource } from '@lagoshny/ngx-hal-client';
+
+export class SelectionProcess extends Resource {
+    id: string;
+    vacancy: string;
+ 
+    uri: string;
+
+    constructor(values: object = {}) {
+        super();
+        Object.assign(this as any, values)
+    }
+}
