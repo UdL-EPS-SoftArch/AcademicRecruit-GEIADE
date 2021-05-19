ed232d94df52feee37bab29fa72bc01045120c56
miquelribes <46230311+mikiribe14@users.noreply.github.com>
2021-05-19 17:23:29 +0200

    Defined Participat attributes

--- src/app/participant/participant.ts
+++ src/app/participant/participant.ts
@@ -1,8 +1,13 @@
 import { Resource } from '@lagoshny/ngx-hal-client';
+import {User} from "../login-basic/user";
+import {SelectionProcess} from "../selection-process/selection-process";
+
 
 export class Participant extends Resource {
     id: string; //int
     role: string; //one of three roles.
+    selectionProcess: SelectionProcess;
+    user: User;
 
     constructor(values: object = {}) {
         super();
