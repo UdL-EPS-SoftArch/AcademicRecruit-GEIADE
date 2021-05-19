530ad7b9a6b589ae45826e5b20142565aca512d4
bilaluali <bilaluali98@gmail.com>
2021-05-19 20:58:13 +0200

    Defined selection process delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { DocumentCreateComponent } from './document/document-create/document-cre
 import { DocumentEditComponent } from './document/document-edit/document-edit.component';
 import { SelectionProcessEditComponent } from './selection-process/selection-process-edit/selection-process-edit.component';
 import { SelectionProcessDetailComponent } from './selection-process/selection-process-detail/selection-process-detail.component';
+import { SelectionProcessDeleteComponent } from './selection-process/selection-process-delete/selection-process-delete.component';
 
 @NgModule({
   declarations: [
@@ -54,7 +55,8 @@ import { SelectionProcessDetailComponent } from './selection-process/selection-p
     SelectionProcessDetailComponent,
     DocumentListComponent,
     DocumentCreateComponent,
-    DocumentEditComponent
+    DocumentEditComponent,
+    SelectionProcessDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/selection-process/selection-process-delete/selection-process-delete.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SelectionProcess } from '../selection-process';
+import { SelectionProcessService } from '../selection-process.service';
+
+@Component({
+  selector: 'app-selection-process-delete',
+  templateUrl: './selection-process-delete.component.html',
+  styleUrls: []
+})
+export class SelectionProcessDeleteComponent implements OnInit {
+  public selectionProcess: SelectionProcess = new SelectionProcess();
+  private id: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private selectionProcessService: SelectionProcessService
+              ) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.selectionProcessService.get(this.id).subscribe(
+      selectionProcess => this.selectionProcess = selectionProcess);
+  }
+
+  delete(): void {
+    this.selectionProcessService.delete(this.selectionProcess).subscribe(
+      () => {
+        () => this.router.navigate(['selectionProcesses'])
+      });
+  }
+
+}
